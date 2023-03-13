082b04af978f26b84ee7d7604d373d7b8f943d78
Ryan <ryanlearyzafra@gmail.com>
2023-03-13 15:41:09 +0100

    🎉 Initial commit

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+/** @type {import("ts-jest").JestCOnfigWithTsJest} */
+export default {
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+    testPathIgnorePatterns: ['dist'],
+    resolver: 'jest-ts-webcompat-resolver',
+    collectCoverageFrom: ['src/**/*.ts'],
+    coveragePathIgnorePatterns: ['index.ts', 'app.ts', 'routers'],
+};
