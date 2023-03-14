64e37a21eef61ee985bf7a9b79a123b49b4ea007
Ryan <ryanlearyzafra@gmail.com>
2023-03-14 15:36:35 +0100

    ✨ Add connection to database

---
+++ src/db/db.connect.ts
@@ -0,0 +1,15 @@
+import mongoose from 'mongoose';
+import { config } from '../config';
+import createDebug from 'debug';
+const debug = createDebug('DB: ');
+
+const { cluster, name, passwd, user } = config;
+
+export const dbConnect = (env?: string) => {
+  const finalEnv = env || process.env.NODE_ENV;
+  const finalDBName = finalEnv === 'test' ? name + '_Testing' : name;
+
+  const uri = `mongodb+srv://${user}:${passwd}@${cluster}/${finalDBName}?retryWrites=true&w=majority`;
+  debug(`Connected to: ${uri}`);
+  return mongoose.connect(uri);
+};
