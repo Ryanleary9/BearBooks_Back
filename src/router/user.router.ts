9183062f87d804b2c030b87b9fb25624470efe40
Ryan <ryanlearyzafra@gmail.com>
2023-03-15 12:36:31 +0100

    🐛  Fix bugs on other files for controller

--- src/router/user.router.ts
+++ src/router/user.router.ts
@@ -1,7 +1,13 @@
 import { Router } from 'express';
-import { UserMongoRepo } from '../repository/user/user.mongo.repo';
+import { UserMongoRepo } from '../repository/user/user.mongo.repo.js';
+import { UsersController } from '../controller/user.controller.js';
+import { AuthInterceptor } from '../interceptors/auth.interceptors.js';
 
 // eslint-disable-next-line new-cap
 export const userRouter = Router();
 const userRepo = UserMongoRepo.getInstance();
-const controller;
+const controller = new UsersController(userRepo);
+const interceptor = new AuthInterceptor(userRepo);
+
+userRouter.post('/register', controller.register.bind(controller));
+userRouter.post('/login', controller.login.bind(controller));
